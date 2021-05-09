0eea0e88a50af0fe904f0120df06fe4c189646f2
vladyslav.rachkoblit <raven73093@gmail.com>
2021-05-09 14:07:17 +0300

    Color Noise in Circle

--- shader.js
+++ shader.js
@@ -1,7 +1,8 @@
 const canvasSketch = require('canvas-sketch');
 const createShader = require('canvas-sketch-util/shader');
 const glsl = require('glslify');
-const cnoise2 = require('glsl-noise');
+
+// const cnoise2 = require('glsl-noise');
 
 // Setup our sketch
 const settings = {
@@ -17,15 +18,22 @@ const frag = glsl`
   uniform float aspect;
   varying vec2 vUv;
 
+  #pragma glslify: snoise2 = require('glsl-noise/simplex/3d');
+  #pragma glslify: hsl2rgb = require('glsl-hsl2rgb');
+
   void main () {
-    vec3 colorA = sin(time * 3.0) + vec3(1.0, 0, 0);
-    vec3 colorB = vec3(0, 0.5, 0);
     vec2 center = vUv - 0.5;
     center.x *= aspect;
     float dist = length(center);
+    float alpha = smoothstep(0.25, 0.2475, dist);
 
-    float alpha = smoothstep(0.355, 0.35, dist);
-    vec3 color = mix(colorA, colorB, vUv.x + vUv.y + sin(time));
+    float n = snoise2(vec3(center * 1.5, time * 0.5));
+
+    vec3 color = hsl2rgb(
+      0.6 + n * 0.2,
+      0.5,
+      0.5
+    );
     gl_FragColor = vec4(color, alpha);
   }
 `;
